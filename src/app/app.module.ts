a79efb1f79e31dce6d300fc117b65723a7c5b478
Ash-990 <ash.990.007@gmail.com>
2019-07-02 16:06:21 +0530

    Fix This Plz !

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
-import { CssComponent } from './css/css.component';
-import { JsComponent } from './js/js.component';
-import { ImgComponent } from './img/img.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent,
-    CssComponent,
-    JsComponent,
-    ImgComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule
